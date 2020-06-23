da5b8a99b5d8a951951411ad61f3cbbc1fcf72fc
tundera <61833561+tundera@users.noreply.github.com>
2020-06-23 05:23:22 -0600

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import { serve } from "https://deno.land/std@v0.51.0/http/server.ts";
+
+const envPort = Deno.env.get("PORT");
+const port = envPort ? parseInt(envPort) : 8000;
+
+const s = serve({ port });
+
+console.log(`listening on http://localhost:${port}/ ...`);
+
+for await (const req of s) {
+  req.respond({
+    body: `Hello, Deno (${JSON.stringify(Deno.version)}) in CodeSandbox!\n`
+  });
+}
